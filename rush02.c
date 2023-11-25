9592dc7cccf1a53d4136c589bf6437ea5fe17e08
Gabri177 <152025617+Gabri177@users.noreply.github.com>
2023-11-25 20:22:15 +0100

    Update rush02.c
    add comment about every funccion

--- rush02.c
+++ rush02.c
@@ -12,8 +12,8 @@
 
 #include "rush02.h"
 
-unsigned int hash_function(const char *str) {  //计算哈希值的函数
-    unsigned long hash = 0;
+unsigned int hash_function(const char *str) {  //la funcion con que podemos calcular el valor de itendidad de una cadena
+    unsigned long hash = 0;                    // se puede considerar como se puede calcular la DNI de una cadena
     int c;
 
     while ((c = *str++))
@@ -22,11 +22,11 @@ unsigned int hash_function(const char *str) {  //计算哈希值的函数
     return hash % HASH_SIZE;
 }
 
-void hash_establecion (hashset *hash, char *dict) //初始化哈希函数 将所有数组元素全部变成NULL指针
+void hash_establecion (hashset *hash, char *dict) //inicio del arry de HASH, hacemos todos los elementos en Hash ser igual que NULL para evitar error
 {
     int index;
-    char **final = file_to_str (dict);
-
+    char **final = file_to_str (dict); // esta funcion puede separar el contenido del diccionario y implimirlo de forma que 
+                                            // en todas las lineas pares salen KEYS, en las inpares salen VALUES
     index = 0;
     while (index < HASH_SIZE)
     {
@@ -36,12 +36,12 @@ void hash_establecion (hashset *hash, char *dict) //初始化哈希函数 将所
     index = 0;
     while (final[index] != 0)
     {
-        hash_append_node (hash, final[index], final[index + 1]);
+        hash_append_node (hash, final[index], final[index + 1]); //ponemos KEY y VALUES en Hashmap. 
         index += 2;
     }
 }
 
-int hash_append_node (hashset *hash, char *key, char *contenido) //给哈希表中添加数值
+int hash_append_node (hashset *hash, char *key, char *contenido) //es la funcion mencionada arriba
 {
     int index = hash_function (key);
     node *present;
@@ -66,23 +66,23 @@ int hash_append_node (hashset *hash, char *key, char *contenido) //给哈希表�
     return (TRUE);
 }
 
-void    hash_query (hashset *hash, char *key)
-{
-    int     index = hash_function (key);
-    node    *presente;
+void    hash_query (hashset *hash, char *key) // con esta funcion podemos buscar el VALUE segun el KEY
+{                                
+    int     index = hash_function (key);        // pasamos el KEY al funcion de HASH para obtener su 'DNI';
+    node    *presente;                           
 
-    presente = hash->bucket[index];
-    if (presente && presente->next == NULL)
+    presente = hash->bucket[index];    
+    if (presente && presente->next == NULL)        //si ary[DNI] cuyo next dirige a NULL, signifiga que solo existe este unico elemento, lo devolvemos
     {
         ft_putstr (presente->contenido);
         return ;
     }
     else
     {
-        while (presente)
+        while (presente)                            //si ary[DNI] cuyo next no dirige a NULL, significa que en la misma posicion no solo guarda un valor
         {
-            if (ft_strcmp (key, presente->key) == 0)
-            {
+            if (ft_strcmp (key, presente->key) == 0)    // en este caso hace falta usar un bucle para pasar por este arry con el pontero principio que es
+            {                                                    // justamente presente. la manera de suma para contador es `presente = presete->next`
                 ft_putstr (presente->contenido);
                 return ;
             }
@@ -118,7 +118,7 @@ char    **file_to_str (char *dir)
     return (final);
 }
 
-void hash_display (hashset* hashset)
+void hash_display (hashset* hashset) // esta funcion la usamos solamente para hacer prueba y ver los elementos guardados en Hashmap
 {
     node    *temp;
     int     index;
@@ -140,12 +140,12 @@ void hash_display (hashset* hashset)
     }
 }
 
-void hash_distory (hashset *hashset)
+void hash_distory (hashset *hashset) // la funcion con la que podemos liberar el espacio que hemos solicitado a la memoria
 {
     
 }
 
-void    ft_putstr (char *str)
+void    ft_putstr (char *str) // alternativa de `printf`
 {
     while (*str)
     {
