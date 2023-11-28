8c733345db8f8d59a0097bb48a1cee3472f5df0d
Gabri177 <152025617+Gabri177@users.noreply.github.com>
2023-11-28 13:48:00 +0100

    Add files via upload

---
+++ BSQfin/bsq_pd.c
@@ -0,0 +1,47 @@
+#include "bsq.h"
+
+position    pd_matrix (char *c, int x, int y)
+{
+    int         columna;
+    int         fila;
+    char        min;
+    char        max;
+    position    p;
+
+    columna = 1;
+    fila = 1;
+    max = 1;
+    while (fila < y)
+    {
+        while(columna < x)
+        {
+            if (c[matrix_index(x, columna, fila)] == '1')
+            {
+                min = ft_min(c[matrix_index(x, columna - 1, fila)],c[matrix_index(x, columna, fila - 1)],c[matrix_index(x, columna - 1, fila - 1)]);
+                //write (1, &min, 1);
+                c[matrix_index(x, columna, fila)] = min;
+                if (max < (int)(min - '0'))
+                {
+                    max = min - '0';
+                    p.x = columna;
+                    p.y = fila;
+                    p.len = max;
+                }
+            }
+            columna ++;
+        }
+        columna = 1;
+        fila ++;
+    }
+    return (p);
+}
+
+position    pd_p_upperleft (position lower_right)
+{
+    position ur;
+
+    ur.x = lower_right.x - lower_right.len + 1;
+    ur.y = lower_right.y - lower_right.len + 1;
+    ur.len = lower_right.len;
+    return (ur);
+}
